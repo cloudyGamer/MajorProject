8b7563d0aef7a8fbd4a9e0aff3afc0c858a66318
MagMurphy <32161012+MagMurphy@users.noreply.github.com>
2017-10-10 15:36:17 +0100

    Update Slash.Java.js

--- Slash.Java.js
+++ Slash.Java.js
@@ -117,34 +117,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-///MODAL FOR DATE LETTERS  ///
-//
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 /* Icon Slide */
 
 $(document).on('pageshow', '#index', function(){ 
